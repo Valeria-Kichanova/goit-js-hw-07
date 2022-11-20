c34c9c05da5385496e6da970404040e39db60348
Valeria-Kichanova <kvv.dnipro@gmail.com>
2022-11-20 22:09:57 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryRef = document.querySelector('.gallery');
+
+const getItemMarkup = ({ original, preview, description }) => `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+`;
+const getBigImgMarkup = ({ url, description }) => `
+  <img
+    class="gallery__image"
+    src="${url}"
+    width="1280"
+    alt="${description}"/>
+`;
+const getImagesMarkup = images => images.map(it => getItemMarkup(it)).join('');
+
+galleryRef.innerHTML = getImagesMarkup(galleryItems);
+galleryRef.addEventListener('click', onClickGallery);
+
+function onClickGallery(e) {
+  e.preventDefault();
+  const link = e.target.closest('.gallery__link');
+  const img = link?.querySelector('.gallery__image');
+
+  if (!(link && img)) return;
+  const {
+    alt: description,
+    dataset: { source: url },
+  } = img;
+
+  showBigImg(getBigImgMarkup({ url, description }));
+}
+
+function showBigImg(markup) {
+  const instance = window.basicLightbox.create(markup);
+  instance.show();
+
+  window.addEventListener('keydown', onKeyDownEsc.bind(instance), {
+    once: true,
+  });
+}
+
+function onKeyDownEsc(e) {
+  if (e.key !== 'Escape') return;
+  this.close();
+  console.log(e.key);
+}
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onOpenModalImageClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+    </a>
+    </div>`;
+    })
+    .join('');
+}
+
+function onOpenModalImageClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
